2a777eb351c6498ae61695c7ac0c29a70608d092
ryansuranjana <ryansuranjana23@gmail.com>
2025-01-30 23:19:17 +0800

    setup module

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,28 @@
 import "module-alias/register"
 import express from "express"
+import { promises as fs } from "fs"
+import path from "path"
 
 const app = express()
 
+const modulesDir = path.join(__dirname, "app")
+
+;(async () => {
+  const modules = await fs.readdir(modulesDir)
+  for (const module of modules) {
+    const routesPath = path.join(modulesDir, module, "routes")
+    try {
+      const routeFiles = await fs.readdir(routesPath)
+      for (const file of routeFiles) {
+        const { default: route } = await import(path.join(routesPath, file))
+        app.use(`/${module}`, route)
+      }
+    } catch (err: any) {
+      throw new Error(`Error loading module ${module}: ${err.message}`)
+    }
+  }
+})()
+
 app.listen(3000, () => {
-  console.log("Server is running on http://localhost:3000")
+  console.log(`Server is running on http://localhost:3000`)
 })
